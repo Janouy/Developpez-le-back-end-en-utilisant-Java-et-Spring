38ea94f93de9547f48d96f15eb7a0eb807592348
Jeanne Boutry <jeanne.boutry@hotmail.fr>
2025-06-30 16:19:25 +0200

    add updated_at & created_at to message

--- src/main/java/com/openclassrooms/chatopapi/model/Message.java
+++ src/main/java/com/openclassrooms/chatopapi/model/Message.java
@@ -1,5 +1,10 @@
 package com.openclassrooms.chatopapi.model;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,6 +27,13 @@ public class Message {
 
 	@NotNull
 	private Integer rental_id;
+
+	@CreationTimestamp
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
 
 	public Long getId() {
 		return id;
